c9e2c3b7465929bc304f9a4366c21b87415476c2
Luke Walter <59621677+LukeWalter@users.noreply.github.com>
2023-10-31 20:03:17 -0400

    Updated gba.h formatting

--- gba.h
+++ gba.h
@@ -23,7 +23,7 @@ extern volatile unsigned short *videoBuffer;
 #define OFFSET(x, y, width) ((y) * (width) + (x))
 
 // Display control register
-#define REG_DISPCTL (*(volatile unsigned short *)0x4000000)
+#define REG_DISPCTL (*(volatile unsigned short*) 0x04000000)
 
 // Bits for display control register
 #define MODE(x) ((x) & 7) // Sets GBA video mode (REG_DISPCTL)
@@ -38,11 +38,11 @@ extern volatile unsigned short *videoBuffer;
 #define BG3_ENABLE (BG_ENABLE(3)) // Enables BG3 (REG_DISPCTL)
 
 // Read-only, holds which scanline is being drawn
-#define REG_VCOUNT (*(volatile unsigned short *)0x4000006)
+#define REG_VCOUNT (*(volatile unsigned short*) 0x04000006)
 
 // Color
-#define PALETTE ((unsigned short *)0x5000000)
-#define COLOR(r, g, b) (((r) & 31) | ((g) & 31) << 5 | ((b) & 31) << 10)
+#define BG_PALETTE ((unsigned short*) 0x05000000)
+#define RGB(R, G, B) (((R) & 31) | ((G) & 31) << 5 | ((B) & 31) << 10)
 
 // Checks for collision between two rectangles
 int collision(int x1, int y1, int width1, int height1, int x2, int y2, int width2, int height2);
@@ -51,17 +51,17 @@ int collision(int x1, int y1, int width1, int height1, int x2, int y2, int width
 void waitForVBlank();
 
 // Buttons
-#define REG_BUTTONS (*(volatile unsigned short *)0x04000130) // Buttons down register
-#define BUTTON_A         (1<<0) // Mask for button A (REG_BUTTONS or REG_KEYCNT)
-#define BUTTON_B         (1<<1) // Mask for button B (REG_BUTTONS or REG_KEYCNT)
-#define BUTTON_SELECT    (1<<2) // Mask for button SELECT (REG_BUTTONS or REG_KEYCNT)
-#define BUTTON_START     (1<<3) // Mask for button START (REG_BUTTONS or REG_KEYCNT)
-#define BUTTON_RIGHT     (1<<4) // Mask for RIGHT button (REG_BUTTONS or REG_KEYCNT)
-#define BUTTON_LEFT      (1<<5) // Mask for LEFT button (REG_BUTTONS or REG_KEYCNT)
-#define BUTTON_UP        (1<<6) // Mask for UP button (REG_BUTTONS or REG_KEYCNT)
-#define BUTTON_DOWN      (1<<7) // Mask for DOWN button (REG_BUTTONS or REG_KEYCNT)
-#define BUTTON_RSHOULDER (1<<8) // Mask for RIGHT SHOULDER button (REG_BUTTONS or REG_KEYCNT)
-#define BUTTON_LSHOULDER (1<<9) // Mask for LEFT SHOULDER button (REG_BUTTONS or REG_KEYCNT)
+#define REG_BUTTONS (*(volatile unsigned short*) 0x04000130) // Buttons down register
+#define BUTTON_A         (1 << 0) // Mask for button A (REG_BUTTONS or REG_KEYCNT)
+#define BUTTON_B         (1 << 1) // Mask for button B (REG_BUTTONS or REG_KEYCNT)
+#define BUTTON_SELECT    (1 << 2) // Mask for button SELECT (REG_BUTTONS or REG_KEYCNT)
+#define BUTTON_START     (1 << 3) // Mask for button START (REG_BUTTONS or REG_KEYCNT)
+#define BUTTON_RIGHT     (1 << 4) // Mask for RIGHT button (REG_BUTTONS or REG_KEYCNT)
+#define BUTTON_LEFT      (1 << 5) // Mask for LEFT button (REG_BUTTONS or REG_KEYCNT)
+#define BUTTON_UP        (1 << 6) // Mask for UP button (REG_BUTTONS or REG_KEYCNT)
+#define BUTTON_DOWN      (1 << 7) // Mask for DOWN button (REG_BUTTONS or REG_KEYCNT)
+#define BUTTON_RSHOULDER (1 << 8) // Mask for RIGHT SHOULDER button (REG_BUTTONS or REG_KEYCNT)
+#define BUTTON_LSHOULDER (1 << 9) // Mask for LEFT SHOULDER button (REG_BUTTONS or REG_KEYCNT)
 
 // Button checks
 extern unsigned short oldButtons; // Keeps track of buttons pressed in previous frame
@@ -98,42 +98,42 @@ extern DMA *dma; // Address to first DMA register
 void DMANow(int channel, volatile const void *src, volatile void *dst, unsigned int cnt);
 
 // Timer registers
-#define REG_TM0CNT *(volatile unsigned short*)0x4000102 // Timer 0 control register
-#define REG_TM1CNT *(volatile unsigned short*)0x4000106 // Timer 1 control register
-#define REG_TM2CNT *(volatile unsigned short*)0x400010A // Timer 2 control register
-#define REG_TM3CNT *(volatile unsigned short*)0x400010E // Timer 3 control register
-#define REG_TM0D   *(volatile unsigned short*)0x4000100 // Initial value for timer 0
-#define REG_TM1D   *(volatile unsigned short*)0x4000104 // Initial value for timer 1
-#define REG_TM2D   *(volatile unsigned short*)0x4000108 // Initial value for timer 2
-#define REG_TM3D   *(volatile unsigned short*)0x400010C // Initial value for timer 3
+#define REG_TM0CNT (*(volatile unsigned short*) 0x04000102) // Timer 0 control register
+#define REG_TM1CNT (*(volatile unsigned short*) 0x04000106) // Timer 1 control register
+#define REG_TM2CNT (*(volatile unsigned short*) 0x0400010A) // Timer 2 control register
+#define REG_TM3CNT (*(volatile unsigned short*) 0x0400010E) // Timer 3 control register
+#define REG_TM0D   (*(volatile unsigned short*) 0x04000100) // Initial value for timer 0
+#define REG_TM1D   (*(volatile unsigned short*) 0x04000104) // Initial value for timer 1
+#define REG_TM2D   (*(volatile unsigned short*) 0x04000108) // Initial value for timer 2
+#define REG_TM3D   (*(volatile unsigned short*) 0x0400010C) // Initial value for timer 3
 
 // Timer bits
-#define TIMER_ON     (1<<7) // Enable this timer
-#define TIMER_OFF    (0<<7) // Disable this timer
-#define TM_IRQ       (1<<6) // Trigger an interrupt when this timer overflows
-#define TM_CASCADE   (1<<2) // Cascade preceding timer into this timer (doesn't work for TM0)
+#define TIMER_ON     (1 << 7) // Enable this timer
+#define TIMER_OFF    (0 << 7) // Disable this timer
+#define TM_IRQ       (1 << 6) // Trigger an interrupt when this timer overflows
+#define TM_CASCADE   (1 << 2) // Cascade preceding timer into this timer (doesn't work for TM0)
 #define TM_FREQ_1    (0) // Each GPU clock cycle
 #define TM_FREQ_64   (1) // Each 64 GPU clock cycles
 #define TM_FREQ_256  (2) // Each 256 GPU clock cycles
 #define TM_FREQ_1024 (3) // Each 1024 GPU clock clycles
 
 // Interrupt registers
-#define REG_IME       *(unsigned short*)0x4000208 // Interrupt (master) control register
-#define REG_IE        *(unsigned short*)0x4000200 // Interrupt enable register
-#define REG_IF        *(volatile unsigned short*)0x4000202 // Interrupt flag register
-#define REG_INTERRUPT *((ihp*)0x03007FFC) // Interrupt handler
+#define REG_IME       (*(unsigned short*) 0x04000208) // Interrupt (master) control register
+#define REG_IE        (*(unsigned short*) 0x04000200) // Interrupt enable register
+#define REG_IF        (*(volatile unsigned short*) 0x04000202) // Interrupt flag register
+#define REG_INTERRUPT (*(ihp*) 0x03007FFC) // Interrupt handler
 
 // Interrupt requests
 #define IRQ_VBLANK   (1 << 0) // Enables VBlank interrupts, requires bit 3 in REG_DISPCNT
 #define IRQ_HBLANK   (1 << 1) // Enables HBlank interrupts, requires bit 4 in REG_DISPCNT
 #define IRQ_VCOUNT   (2 << 2) // Enables scanline/VCount interrupts, requires bit 5 in REG_DISPCNT
 #define IRQ_TIMER(x) (1 << ((x % 4) + 3)) // Enables timer x interrupts, requires bit 6 in REG_TMxCNT
-#define IRQ_DMA(x)   (1 << ((x % 4)) + 8) // Enables DMA x interrupts, requires that DMA channel's register
+#define IRQ_DMA(x)   (1 << ((x % 4) + 8)) // Enables DMA x interrupts, requires that DMA channel's register
 #define IRQ_BUTTON   (1 << 12) // Enables button interrupts, requires bit E (and button specifications) in REG_KEYCNT
 
 // Registers used for specific interrupts
-#define REG_DISPSTAT *(unsigned short*)0x4000004 // Display interrupt register
-#define REG_KEYCNT   *(unsigned short*)0x4000132 // Button interrupt register
+#define REG_DISPSTAT (*(unsigned short*) 0x04000004) // Display interrupt register
+#define REG_KEYCNT   (*(unsigned short*) 0x04000132) // Button interrupt register
 
 // Display interrupt requests
 #define DISPSTAT_VBLANK_IRQ   (1 << 3) // Enables VBlank interrupts in REG_DISPSTAT
