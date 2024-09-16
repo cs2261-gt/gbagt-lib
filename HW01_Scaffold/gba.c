632e49ce07b00ac459a80ade86b1ae1855c17c3c
mthies6 <mthies6@gatech.edu>
2024-09-16 14:10:13 -0400

    Added F24 HW01 + HW02 Scaffold
    Added the scaffolds for both hw01 and hw02 for future reference on what aspects of the library are required/helpful at which point in the course. These scaffolds were used in the Fall '24 assignments.

---
+++ HW01_Scaffold/gba.c
@@ -0,0 +1,33 @@
+#include "gba.h"
+
+volatile unsigned short* videoBuffer = (volatile unsigned short*) 0x6000000;
+
+void drawHorizontalLine(int y, int x1, int x2, volatile u16 color) {
+    for (int i = x1; i <= x2; i++) {
+        setPixel(i, y, color);
+    }
+}
+
+void drawVerticalLine(int x, int y1, int y2, volatile u16 color) {
+    for (int i = y1; i <= y2; i++) {
+        setPixel(x, i, color);
+    }
+}
+
+void drawRectangle(int x, int y, int width, int height, volatile u16 color) {
+    for (int i = 0; i < width; i++) {
+        for (int j = 0; j < height; j++) {
+            setPixel(x + i, y + j, color);
+        }
+    }
+}
+
+//TODO: Add this function from Lab03
+void fillScreen(volatile u16 color) {
+
+}
+
+void waitForVBlank() {
+    while (REG_VCOUNT >= 160);
+    while (REG_VCOUNT < 160);
+}
---
+++ HW02_Scaffold/main.c
@@ -0,0 +1,21 @@
+#include "gba.h"
+#include "print.h"
+
+void initialize();
+
+int main() {
+
+    initialize();
+
+    while (1) {
+
+    }
+    
+    return 0;
+
+}
+
+void initialize() {
+    
+    
+}
